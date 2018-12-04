4de2fc95e8f0afe40a3c5b62061d899f5319b2de
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-04 13:02:43 -0500

    create predicate types

---
+++ src/array/types.ts
@@ -0,0 +1,10 @@
+type PredicateFunction<T> = (value: T) => boolean
+type PredicateMatchObject<T> = Partial<T>
+type PredicatePropertyMatch<T, K extends keyof T> = [K, T[K]]
+type PredicateProperty = string
+
+type Predicate<T, K extends keyof T> 
+= PredicateFunction<T>
+| PredicateMatchObject<T>
+| PredicatePropertyMatch<T, K>
+| PredicateProperty
