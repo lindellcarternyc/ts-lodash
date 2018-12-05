3d1416b2530d83eb58dcc785f3eb49b035182f31
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-04 19:11:52 -0500

    @fix linting error about semicolons

--- src/array/fill/fill.ts
+++ src/array/fill/fill.ts
@@ -1,8 +1,8 @@
-const fill = <T, U>($array: T[], value: U, start: number = 0, end?: number): Array<T | U> => {
+const fill = <T, U>($array: Array<T | U>, value: U, start: number = 0, end?: number): Array<T | U> => {
   const endIdx: number = end === undefined ? $array.length : end < 0 ? 0 : end > $array.length ? $array.length : end
 
   for (let startIdx = start >= 0 ? start : 0; startIdx < endIdx; startIdx++) {
-    ;($array as any[])[startIdx] = value
+    $array[startIdx] = value
   }
   return $array
 }
