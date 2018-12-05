29b42b1ade5bce87f7528c6e6818d07439d13d9f
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-04 19:08:24 -0500

    0.6.1

--- src/array/fill/fill.ts
+++ src/array/fill/fill.ts
@@ -2,7 +2,7 @@ const fill = <T, U>($array: T[], value: U, start: number = 0, end?: number): Arr
   const endIdx: number = end === undefined ? $array.length : end < 0 ? 0 : end > $array.length ? $array.length : end
 
   for (let startIdx = start >= 0 ? start : 0; startIdx < endIdx; startIdx++) {
-    ($array as any[])[startIdx] = value
+    ;($array as any[])[startIdx] = value
   }
   return $array
 }
