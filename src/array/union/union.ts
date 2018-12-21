2f24548c3d3f6ada2e6116e15fadfe649b579e91
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-20 22:30:16 -0500

    use uniq module in union

--- src/array/union/union.ts
+++ src/array/union/union.ts
@@ -1,35 +1,11 @@
-interface IBooleanMap {[key: string]: boolean}
-
-const unique = <T>(array: T[], result: T[] = [], index: number = 0, boolMap: IBooleanMap = { }): { result: T[], boolMap: IBooleanMap }  => {
-  if ( index >= array.length ) {
-    return { result, boolMap }
-  }
-  
-  const item = array[index]
-  const key = JSON.stringify(item)
-
-  if ( boolMap[key] !== true ) {
-    return unique(array, result.concat(item), index + 1, { ...boolMap, [key]: true })
-  }
-
-  return unique(array, result, index + 1, boolMap)
-}
+import { handleUniq } from '../uniq'
 
 const union = <T>(array: T[], ...otherArrays: T[][]): T[] => {
-  const { 
-    result,
-    boolMap
-  } = unique(array)
-  
-  let updatedResult = [...result]
-  let updatedBoolMap = { ...boolMap }
+  let [ result, map ] = handleUniq(array)
   for ( const otherArray of otherArrays ) {
-    const updatedUnique = unique(otherArray, [], 0, updatedBoolMap)
-    updatedResult = updatedResult.concat(updatedUnique.result)
-    updatedBoolMap = updatedUnique.boolMap
+    [ result, map ] = handleUniq(otherArray, 0, result, map)
   }
-
-  return updatedResult
+  return result
 }
 
 export default union
