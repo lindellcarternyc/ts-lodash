d9145742b187ad0c158fd584323cd2f733ccac37
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-21 16:06:43 -0500

    @BUGFIX/stop ignoring previous result if index = 0

--- src/array/uniq/uniqWith.ts
+++ src/array/uniq/uniqWith.ts
@@ -1,3 +1,5 @@
+import { Comparator } from '../../generics'
+
 interface IUniqValue<T> {
   value: T
 }
@@ -6,32 +8,21 @@ interface IUniqValueMap<T> {
   [key: string]: IUniqValue<T>
 }
 
-type Comparator<T> = (item1: T, item2: T) => boolean
-
 const handleUniqWith = <T>(array: T[], comparator: Comparator<T>, index: number = 0, result: T[] = [], uniqValueMap: IUniqValueMap<T> = { }): [T[], IUniqValueMap<T>] => {
   if ( index >= array.length ) {
     return [
       result,
       uniqValueMap
     ]
-  } else if ( index === 0 ) {
-    const item = array[0]
-    const key = JSON.stringify(item)
-    return handleUniqWith(array, comparator, index + 1, [ item ], { [key]: { value: item }})
   } else {
     const item = array[index]
     const key = JSON.stringify(item)
 
     let doesMatch: boolean = false
     for ( const uniqKey in uniqValueMap ) {
-      if ( uniqKey === key ) { 
+      if ( comparator(uniqValueMap[uniqKey].value, item) ) {
         doesMatch = true
         break
-      } else {
-        if ( comparator(uniqValueMap[uniqKey].value, item) ) {
-          doesMatch = true
-          break
-        }
       }
     }
     if ( doesMatch ) {
